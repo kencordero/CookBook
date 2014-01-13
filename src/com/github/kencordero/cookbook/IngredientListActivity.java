d6b4b77146f30b33dabe1ab256962f50572da9d0
Kenneth Cordero <kencordero@gmail.com>
2014-01-12 23:44:15 -0600

    Adding refs for master-detail activity

---
+++ src/com/github/kencordero/cookbook/IngredientListActivity.java
@@ -0,0 +1,18 @@
+package com.github.kencordero.cookbook;
+
+import android.support.v4.app.Fragment;
+
+public class IngredientListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected int getLayoutResId() {
+		return R.layout.activity_masterDetail;
+	}
+
+}
